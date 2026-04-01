4c6be4bd73c1f03863ac5c185ea039a094b38c6f
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-04-01 11:46:37 +0300

    todo5

--- utils/parser.js
+++ utils/parser.js
@@ -4,7 +4,6 @@ export function parseNumbers(input) {
   const numbers = _.map(input, (str) => Number(str));
   return _.compact(numbers);
 }
-
 export function isValidOperation(operation) {
   const validOps = ["add", "subtract", "multiply", "divide"];
   return _.includes(validOps, operation);
